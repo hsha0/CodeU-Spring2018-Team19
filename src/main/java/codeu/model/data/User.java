bd8c8bbf818c884f7010c06a30ef9241e92d49ac
Han Shao <hshao@oberlin.edu>
2018-03-25 01:58:26 -0400

    Add new information to User.java and initialize them.

--- src/main/java/codeu/model/data/User.java
+++ src/main/java/codeu/model/data/User.java
@@ -19,45 +19,91 @@ import java.util.UUID;
 
 /** Class representing a registered user. */
 public class User {
-  private final UUID id;
-  private final String name;
-  private final String password;
-  private final Instant creation;
+    private final UUID id;
+    private final String name;
+    private final String password;
+    private final Instant creation;
+    private final Integer Age;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String phoneNum;
+    private final String bio;
 
-  /**
-   * Constructs a new User.
-   *
-   * @param id the ID of this User
-   * @param name the username of this User
-   * @param password the password of this User
-   * @param creation the creation time of this User
-   */
-  public User(UUID id, String name, String password, Instant creation) {
-     this.id = id;
-     this.name = name;
-     this.password = password;
-     this.creation = creation;
-  }
+    /**
+     * Constructs a new User.
+     *
+     * @param id
+     *            the ID of this User
+     * @param name
+     *            the username of this User
+     * @param password
+     *            the password of this User
+     * @param creation
+     *            the creation time of this User
+     */
+    public User(UUID id, String name, String password, Instant creation) {
+	this.id = id;
+	this.name = name;
+	this.password = password;
+	this.creation = creation;
+	
+	/** Initializes other information for profile pages. */
+	this.Age = 0;
+	this.firstName = "First name";
+	this.lastName = "Last name";
+	this.email = "no-reply@example.com";
+	this.phoneNum = "123-456-7890";
+	this.bio = "Hello World!";
+    }
 
-  /** Returns the ID of this User. */
-  public UUID getId() {
-    return id;
-  }
+    /** Returns the ID of this User. */
+    public UUID getId() {
+	return id;
+    }
 
-  /** Returns the username of this User. */
-  public String getName() {
-    return name;
-  }
+    /** Returns the username of this User. */
+    public String getName() {
+	return name;
+    }
 
-  /**
-   * Returns the password of this User.
-   */
-  public String getPassword() {
-    return password;
-  }
+    /** Returns the password of this User. */
+    public String getPassword() {
+	return password;
+    }
 
-  /** Returns the creation time of this User. */
-  public Instant getCreationTime() {
-    return creation;
-  }
+    /** Returns the creation time of this User. */
+    public Instant getCreationTime() {
+	return creation;
+    }
+    
+    /** Returns the age of this User. */
+    public Integer getAge() {
+	return Age;
+    }
+    
+    /** Returns the first name of this User. */
+    public String getFirstName() {
+	return firstName;
+    }
+
+    /** Returns the last name of this User. */
+    public String getLastName() {
+	return lastName;
+    }
+
+    /** Returns the email of this User. */
+    public String getEmail() {
+	return email;
+    }
+
+    /** Returns the phone number of this User. */
+    public String getPhoneNum() {
+	return phoneNum;
+    }
+    
+    /** Returns the bio of this User. */
+    public String getBio() {
+	return bio;
+    }
 }
