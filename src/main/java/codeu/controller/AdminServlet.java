2c4443f5a9f6340701ea3c3b42bcec4a36f4493a
Unknown <zeineddine.jad@gmail.com>
2018-05-22 23:22:52 -0700

    Mockup of post

--- src/main/java/codeu/controller/AdminServlet.java
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -45,7 +45,37 @@ public class AdminServlet extends HttpServlet {
   
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-    
+    String banrate = request.getParameter("banrate");
+    String username = request.getParameter("username");
+    String adminname = (String) request.getSession().getAttribute("user");
+
+    if (adminname == null) {
+      request.setAttribute("error", "User not logged in.");
+      response.sendRedirect("/login");
+      return;
+    }
+
+    if (username.compareTo("") == 0) {
+      request.setAttribute("error", "Invalid user.");
+      response.sendRedirect("/admin");
+      return;
+    }
+
+    User admin = userStore.getUser(adminname);
+    User user = userStore.getUser(username);
+    if (admin.isSuperUser()) {
+      if (banrate.compareTo("ban") == 0) {
+        userStore.deleteUser(user);
+      } else {
+        Integer rate = Integer.parseInt(banrate);
+        user.setRateLimit(rate);
+      }
+    }
+
+    else {
+      request.setAttribute("error", "Not administrator.");
+      return;
+    }
   }
   
 }
